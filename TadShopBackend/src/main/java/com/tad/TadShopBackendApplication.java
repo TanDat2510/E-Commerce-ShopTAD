ffff138700ad76e306cffcac541d9c63c3e0ff0c
Tan Dat <dattanlee@gmail.com>
2025-10-14 19:21:35 +0700

    first commit TadShopBackend

---
+++ TadShopBackend/src/main/java/com/tad/TadShopBackendApplication.java
@@ -0,0 +1,18 @@
+package com.tad;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadShopBackendApplication.class, args);
+	}
+
+    @PostConstruct
+    public void Test(){
+        System.out.println("Toi la dat");
+    }
+}
---
+++ TadShopBackend/src/test/java/com/tad/TadShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
